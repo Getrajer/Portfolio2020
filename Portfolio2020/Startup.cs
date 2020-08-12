e54f4ca4e302bb83adfa8f90f4a9452e20993f1e
getrajer533 <getrajer533@gmail.com>
2020-08-12 10:49:12 +0100

    Changes

--- Portfolio2020/Startup.cs
+++ Portfolio2020/Startup.cs
@@ -40,8 +40,6 @@ namespace Portfolio2020
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
